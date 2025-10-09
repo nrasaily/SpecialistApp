93ebe9e6dac8a53b7f8e0cac16acf2a461bb5019
nrasaily <nareshsathi79@gmail.com>
2025-10-09 02:18:11 -0400

    new project created

---
+++ New104Mdi/New104Mdi/Views/StepsView.swift
@@ -0,0 +1,18 @@
+//
+//  StepsView.swift
+//  New104Mdi
+//
+//  Created by Nar Rasaily on 10/8/25.
+//
+
+import SwiftUI
+
+struct StepsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepsView()
+}
