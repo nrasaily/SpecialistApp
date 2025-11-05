63568f588286218522e971ddc775df579b6890c1
nrasaily <nareshsathi79@gmail.com>
2025-11-04 22:13:21 -0500

    finalproject

---
+++ Meta/MetaReservations/MetaReservations/Views/CustomNavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNavigationView.swift
+//  MetaReservations
+//
+//  Created by Nar Rasaily on 11/4/25.
+//
+
+import SwiftUI
+
+struct CustomNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavigationView()
+}
