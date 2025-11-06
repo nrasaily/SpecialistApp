fa350a6b2a851d209e3c7168cc1e2d2302a34965
nrasaily <nareshsathi79@gmail.com>
2025-11-06 02:38:34 -0500

    added

---
+++ BookManager/BookManager/ViewModel/AddEditBookViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  AddEditBookViewModel.swift
+//  BookManager
+//
+//  Created by Nar Rasaily on 11/5/25.
+//
+
---
+++ Meta/MetaReservations/MetaReservations/Views/ObservedObject.swift
@@ -0,0 +1,18 @@
+//
+//  ObservedObject.swift
+//  MetaReservations
+//
+//  Created by Nar Rasaily on 11/5/25.
+//
+
+import SwiftUI
+
+struct ObservedObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservedObject()
+}
